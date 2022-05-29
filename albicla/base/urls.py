74420b61c18dad05d5d1f1bb48394741f951547c
Bunnicz <kpodgorski367@gmail.com>
2022-05-29 20:54:29 +0200

    Add user login/logout

--- albicla/base/urls.py
+++ albicla/base/urls.py
@@ -1,13 +1,13 @@
-
 from django.urls import path
 from . import views
 
+# name is used in template tags {% url 'name' %}
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('room/<str:pk>/', views.room, name='room'),
-    
-    path('create-room/', views.createRoom, name="create-room"),
-    path('update-room/<str:pk>', views.updateRoom, name="update-room"),
-    path('delete-room/<str:pk>', views.deleteRoom, name="delete-room"),
-    
-    ]
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("", views.home, name="home"),
+    path("room/<str:pk>/", views.room, name="room"),
+    path("create-room/", views.createRoom, name="create-room"),
+    path("update-room/<str:pk>", views.updateRoom, name="update-room"),
+    path("delete-room/<str:pk>", views.deleteRoom, name="delete-room"),
+]
--- albicla/base/views.py
+++ albicla/base/views.py
@@ -1,29 +1,55 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 
 from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 # pk - primary key
+# q - querry
 
-# rooms = [
-#     {'id':1, 'name': 'Lets learn python!'},
-#     {'id':2, 'name': 'Design with me'},
-#     {'id':3, 'name': 'Frontend devs'},
-# ]
+
+def loginPage(request):  # Cant be just login - buildIn function
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            # flash messages
+            messages.error(request, "User does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Username OR password does not exist!")
+
+    context = {}
+    return render(request, "base/login_register.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("home")
 
 
 def home(request):
-    q = request.GET.get('q') if request.GET.get('q') != None else ""
-    
-    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
-                                Q(name__icontains=q) |
-                                Q(description__icontains=q)
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)
     )
 
     topics = Topic.objects.all()
-    
+
     room_count = rooms.count()
 
     context = {"rooms": rooms, "topics": topics, "room_count": room_count}
