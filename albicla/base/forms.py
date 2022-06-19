cdb5544f3162f6a02c361ffe1ee6dc0bb03ff40b
Bunnicz <kpodgorski367@gmail.com>
2022-06-19 16:27:34 +0200

    Theme Instalation

--- albicla/base/forms.py
+++ albicla/base/forms.py
@@ -1,11 +1,10 @@
 from django.forms import ModelForm
 from .models import Room
 
+
 class RoomForm(ModelForm):
     class Meta:
-        # Create the form based on the meta data from Room model 
+        # Create the form based on the meta data from Room model
         model = Room
-        fields = '__all__'
-    
-    
-
+        fields = "__all__"
+        exclude = ["host", "participants"]
