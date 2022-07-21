81bc1d9877967cef493329a56ae9bdbd58450cc2
Bunnicz <kpodgorski367@gmail.com>
2022-07-21 21:18:49 +0200

    Local settings dotenv setup

--- albicla/albicla/settings.py
+++ albicla/albicla/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+# coding: utf-8
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(hxf)4kf6oktupl%+d4%c_jz8bjn+(y+zszxaosc^(9(ir_uqg"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -88,6 +90,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        # "USER": config("DATABASE_USER"),
+        # "PASSWORD": config("DATABASE_PASS"),
     }
 }
 
